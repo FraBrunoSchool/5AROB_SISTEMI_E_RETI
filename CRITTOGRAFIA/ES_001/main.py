8703c1fac948fc6f2a812948835575cbd470a33e
Francesco Bruno <francesco.bruno@itiscuneo.eu>
2020-10-29 08:59:38 +0100

    vernam

--- CRITTOGRAFIA/ES_001/main.py
+++ CRITTOGRAFIA/ES_001/main.py
@@ -2,14 +2,32 @@ dictLet = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 
            "P": 13, "Q": 14, "R": 15, "S": 16, "T": 17, "U": 18, "V": 19, "Z": 20}
 dictNum = {}
 
+
 def main():
     dictLetInNum()
-    chiave = "ITIDELPOZZO"
+    chiave = "ITISDELPOZZO"
     messaggio = "CIAO"
 
-    print(dictLet)
-    print(dictNum)
-    print(stringaInNumber(messaggio))
+    # print(dictLet)
+    # print(dictNum)
+    msgNum = stringaInNumber(messaggio)
+    keyNum = stringaInNumber(chiave)
+    print(f"Messaggio: {msgNum} + Chiave {keyNum}")
+    somma = sommaKeyMsg(keyNum, msgNum)
+    print(somma)
+    mod = modulo(somma)
+    print(mod)
+    StrLettere = NumToLet(mod)
+    print(StrLettere)
+
+    StrLettereCifr = stringaInNumber(StrLettere)
+    print(StrLettereCifr)
+    sotr = sottrKeyMsg(keyNum, StrLettereCifr)
+    print(sotr)
+    modCifr = modulo(sotr)
+    print(modCifr)
+    StrLettereDecifrato = NumToLet(modCifr)
+    print(StrLettereDecifrato)
 
 
 def dictLetInNum():
@@ -24,69 +42,39 @@ def stringaInNumber(stringa):
     return num
 
 
+def sommaKeyMsg(chiave, messaggio):
+    somma = []
+    if len(chiave) > len(messaggio):
+        for count, n in enumerate(messaggio):
+            somma.append(n + chiave[count])
+    else:
+        print("chive non abbastanza lunga")
+    return somma
+
+
+def modulo(somma):
+    mod = []
+    for el in somma:
+        mod.append(el % len(dictLet))
+    return mod
+
+
+def NumToLet(mod):
+    StrLettere = ""
+    for el in mod:
+        StrLettere += dictNum[el]
+    return StrLettere
+
+
+def sottrKeyMsg(chiave, messaggio):
+    somma = []
+    if len(chiave) > len(messaggio):
+        for count, n in enumerate(messaggio):
+            somma.append(n - chiave[count])
+    else:
+        print("chive non abbastanza lunga")
+    return somma
+
+
 if __name__ == '__main__':
     main()
-
-"""
-def main():
-    cifratura()
-
-
-def cifratura():
-    # dizionario alfabeto
-    dict = {
-        'A': 0,
-        'B': 1,
-        'C': 2,
-        'D': 3,
-        'E': 4,
-        'F': 5,
-        'G': 6,
-        'H': 7,
-        'I': 8,
-        'L': 9,
-        'M': 10,
-        'N': 11,
-        'O': 12,
-        'P': 13,
-        'Q': 14,
-        'R': 15,
-        'S': 16,
-        'T': 17,
-        'U': 18,
-        'V': 19,
-        'Z': 20
-    }
-    # dichiarazione variabili
-    chiave = "ITIDELPOZZO"
-    messaggio = "CIAO"
-    chiaveCifrata = []
-    messaggioCifrato = []
-    sommaMexChiave = []
-    offset = []
-
-    # cifratura chiave
-    for c in chiave:
-        chiaveCifrata.append(str(dict.get(c)))
-    print(chiaveCifrata)
-
-    # cifratura messaggio
-    for i in messaggio:
-        messaggioCifrato.append(str(dict.get(i)))
-    print(messaggioCifrato)
-
-    # somma messaggio e chiave
-    for a in range(0, len(messaggioCifrato)):
-        sommaMexChiave.append(str(int(chiaveCifrata[a]) + int(messaggioCifrato[a]) % 21))
-        if (int(sommaMexChiave[a]) > 21):
-            offset.append(1)
-        else:
-            offset.append(0)
-    print(offset)
-    print(sommaMexChiave)
-
-
-if __name__ == '__main__':
-    cifratura()
-    
-    """
